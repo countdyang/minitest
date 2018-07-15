d1459b8be6f3bf0f90f2c2dbf4a135afdf19b65c
countdyang <countdyang@hotmail.com>
2018-07-15 10:30:02 +0800

    debug

--- Minicodingtest/src/main/java/test.java
+++ Minicodingtest/src/main/java/test.java
@@ -8,11 +8,11 @@ public class test {
             System.out.println("Impossible.");
         }
         for(int i = 1; i <= total; i++){
-            if((i%3 == 0)&&(i%5 == 0)){
+            if((i%3 == 0)&&(i%5 == 0)){// meet both requirement
                 System.out.println("FizzBuzz");
-            }else if(i%3 == 0){
+            }else if(i%3 == 0){ // multiples of 3
                 System.out.println("Fizz");
-            }else if(i%5 == 0){
+            }else if(i%5 == 0){// multiples of 5
                 System.out.println("Buzz");
             }else{
                 System.out.println(i);
@@ -25,11 +25,19 @@ public class test {
             System.out.println("Impossible.");
         }
         for(int i = 1; i <= total; i++){
-            if((i%3 == 0)&&(i%5 == 0)){
+            boolean fizz = false;// whether a number meets stage 2 fizz requirement
+            boolean buzz = false;// whether a number meets stage 2 buzz requirement
+            if((i%3 == 0) || checkFizzBuzzCondition(i,3)){
+                 fizz = true;
+            }
+            if((i%5 == 0) || checkFizzBuzzCondition(i,5)){
+                buzz = true;
+            }
+            if(fizz && buzz){// a number that meets both requirements
                 System.out.println("FizzBuzz");
-            }else if((i%3 == 0) || checkFizzBuzzCondition(i,3)){
+            }else if(fizz == true && buzz == false){// only meet fizz requirement
                 System.out.println("Fizz");
-            }else if((i%5 == 0) || checkFizzBuzzCondition(i,5)){
+            }else if(fizz == false && buzz == true){// only meet buzz requirement
                 System.out.println("Buzz");
             }else{
                 System.out.println(i);
@@ -37,6 +45,7 @@ public class test {
         }
     }
 
+    // Whether a number has a divisor in it.
     public boolean checkFizzBuzzCondition(int dividen, int divisor){
         while(dividen % 10 != 0){
             int temp = dividen % 10;
---
+++ Minicodingtest/src/test/java/unitTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+/**
+ * Created by countdyang on 7/15/2018.
+ */
+public class unitTest {
+
+
+    @Test
+	public void test(){
+        test temp = new test();
+        temp.gameOutputStage1(100);// for stage1
+        System.out.println("——————————————————split line————————————————————————————");
+        temp.gameOutputStage2(100);// for stage2
+
+	}
+}
